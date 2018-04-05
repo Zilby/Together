ca4a6e17ed184067f791f5c2b0d60bb7df4de51b
Zilby <azilby@gmail.com>
2018-04-05 15:54:19 -0400

    Added trailer nonsense

---
+++ Together/Assets/Scripts/Trailer/TrailerBeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrailerBeat : Beat
+{
+	protected override void Start() 
+	{
+		base.Start();
+		StartCoroutine(GrowAndStress());
+	}
+
+
+	private IEnumerator GrowAndStress() 
+	{
+		for (;;)
+		{
+			yield return new WaitForSecondsRealtime(60f / 80f);
+			love += 1.2f;
+			frequency -= 1;
+		}
+	}
+
+	public override void ScaleLove()
+	{
+		GetComponent<Transform>().localScale = (GetComponent<Transform>().localScale / 2.2f) * ((love / 25f) + 1);
+	}
+}
+
