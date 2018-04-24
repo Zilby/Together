47237c2a0982b0dc4320c8a81a843cf1a4fe8d29
Zilby <azilby@gmail.com>
2018-04-24 04:52:16 -0400

    formatting

--- Together/Assets/Scripts/Game/GameController.cs
+++ Together/Assets/Scripts/Game/GameController.cs
@@ -7,7 +7,8 @@ using UnityEngine.SceneManagement;
 /// <summary>
 /// Controls the game, but most notably the ending sequence. 
 /// </summary>
-public class GameController : MonoBehaviour {
+public class GameController : MonoBehaviour
+{
 
 	/// <summary>
 	/// The player heart.
@@ -36,7 +37,7 @@ public class GameController : MonoBehaviour {
 
 	public GameObject gameover;
 	private bool over;
-	public Text dialogue; 
+	public Text dialogue;
 	// set the response to the location you want it to go to after
 	private int location;
 
@@ -58,9 +59,9 @@ public class GameController : MonoBehaviour {
 	/// <summary>
 	/// The dialogue texts.
 	/// </summary>
-    private List<string> dialogueTexts = new List<string>();
+	private List<string> dialogueTexts = new List<string>();
 
-    private bool cracking;
+	private bool cracking;
 
 	/// <summary>
 	/// Whether or not something is currently fading in. 
@@ -75,7 +76,7 @@ public class GameController : MonoBehaviour {
 	/// <summary>
 	/// Both the speed and frequency of the falling text. 
 	/// </summary>
-    public static float speed; 
+	public static float speed;
 
 	/// <summary>
 	/// Text indicating that the player should press the spacebar. 
@@ -90,7 +91,7 @@ public class GameController : MonoBehaviour {
 	/// <summary>
 	/// The list of falling texts. 
 	/// </summary>
-	public List<GameObject> texts; 
+	public List<GameObject> texts;
 
 	/// <summary>
 	/// Timed counter indicating the next falling text. 
@@ -102,20 +103,21 @@ public class GameController : MonoBehaviour {
 	/// </summary>
 	[SerializeField]
 	private int index;
-    public static bool ending;
+	public static bool ending;
 
-	public void Start() {
+	public void Start()
+	{
 		over = false;
 		location = 0;
 		letterPause = 0.005f;
 		typing = false;
 		skip = false;
-        gameover.SetActive(false);
-        ending = false;
+		gameover.SetActive(false);
+		ending = false;
 		pressSpace = false;
-        speed = 1;
-        cracking = false;
-		StartCoroutine (Arrows ());
+		speed = 1;
+		cracking = false;
+		StartCoroutine(Arrows());
 
 
 		index = 0;
@@ -140,35 +142,48 @@ public class GameController : MonoBehaviour {
 		dialogueTexts.Add("But you don't");
 		dialogueTexts.Add("Because even if you'll never get back together");
 		dialogueTexts.Add("Maybe someday you'll be able to feel the same love again");
-    }
+	}
 
-	void Update() {
-		if (Input.GetKey ("escape")) {
-			Application.Quit ();
+	void Update()
+	{
+		if (Input.GetKey("escape"))
+		{
+			Application.Quit();
 		}
-		if (fadeIn && !pressSpace) {
-			arrows.color = Color.Lerp (arrows.color, new Color (1, 1, 1, 1), 1.2f * Time.deltaTime);
-			if (arrows.color.a > 0.95) {
+		if (fadeIn && !pressSpace)
+		{
+			arrows.color = Color.Lerp(arrows.color, new Color(1, 1, 1, 1), 1.2f * Time.deltaTime);
+			if (arrows.color.a > 0.95)
+			{
 				fadeIn = false;
 				fadeOut = true;
 			}
-		} else if (fadeOut && !pressSpace) {
-			arrows.color = Color.Lerp (arrows.color, new Color (1, 1, 1, 0), 1.5f * Time.deltaTime);
-			if (arrows.color.a < 0.01) {
+		}
+		else if (fadeOut && !pressSpace)
+		{
+			arrows.color = Color.Lerp(arrows.color, new Color(1, 1, 1, 0), 1.5f * Time.deltaTime);
+			if (arrows.color.a < 0.01)
+			{
 				fadeOut = false;
-                arrows.enabled = false;
+				arrows.enabled = false;
 			}
 		}
-		if (pressSpace) {
-			if (fadeIn) {
-				space.color = Color.Lerp (space.color, new Color (1, 1, 1, 1), 1.2f * Time.deltaTime);
-				if (space.color.a > 0.95) {
+		if (pressSpace)
+		{
+			if (fadeIn)
+			{
+				space.color = Color.Lerp(space.color, new Color(1, 1, 1, 1), 1.2f * Time.deltaTime);
+				if (space.color.a > 0.95)
+				{
 					fadeIn = false;
 					fadeOut = true;
 				}
-			} else if (fadeOut) {
-				space.color = Color.Lerp (space.color, new Color (1, 1, 1, 0), 1.5f * Time.deltaTime);
-				if (space.color.a < 0.01) {
+			}
+			else if (fadeOut)
+			{
+				space.color = Color.Lerp(space.color, new Color(1, 1, 1, 0), 1.5f * Time.deltaTime);
+				if (space.color.a < 0.01)
+				{
 					fadeOut = false;
 					fadeIn = true;
 				}
@@ -176,62 +191,68 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	public void FixedUpdate() {
-		if(over) {
+	public void FixedUpdate()
+	{
+		if (over)
+		{
 
-        } else if (heart == null) {
-            foreach(GameObject obj in texts)
-            {
-                if(obj)
-                {
-                    obj.SetActive(false);
-                }
-            }
+		}
+		else if (heart == null)
+		{
+			foreach (GameObject obj in texts)
+			{
+				if (obj)
+				{
+					obj.SetActive(false);
+				}
+			}
 			stress.text = "";
 			compassion.text = "";
 			love.text = "";
-			string[] messages = { "Again?", "Maybe Next Time", "Broken", "The End", 
+			string[] messages = { "Again?", "Maybe Next Time", "Broken", "The End",
 				"It Was Never Real", "Get Over It", "Love Is A Myth" };
-            gameover.SetActive(true);
-			gameover.GetComponent<Button>().GetComponent<Text>().text = messages [Random.Range (0, 7)];
+			gameover.SetActive(true);
+			gameover.GetComponent<Button>().GetComponent<Text>().text = messages[Random.Range(0, 7)];
 			over = true;
-		} else {
-            if(ending)
-            {
-                if(location == 20)
-                {
-                    StartCoroutine(End());
-                }
-            }
+		}
+		else
+		{
+			if (ending)
+			{
+				if (location == 20)
+				{
+					StartCoroutine(End());
+				}
+			}
 			else
-            {
-                if (counter <= 0 && index < texts.Count)
-                {
-                    texts[index].SetActive(true);
-                    counter = 200 / speed;
-                    index++;
-                    if (index % 5 == 0)
-                    {
-                        speed += 0.03f;
-                    }
-                }
-                else if (index == texts.Count && !texts[texts.Count - 1])
-                {
-                    ending = true;
-                    heart.end = true;
-                    dialogue.gameObject.SetActive(true);
-                    Talk();
-                }
-                else
-                {
-                    counter--;
-                }
-            }
-            stress.text = "Stress: " + (100 - heart.Frequency);
+			{
+				if (counter <= 0 && index < texts.Count)
+				{
+					texts[index].SetActive(true);
+					counter = 200 / speed;
+					index++;
+					if (index % 5 == 0)
+					{
+						speed += 0.03f;
+					}
+				}
+				else if (index == texts.Count && !texts[texts.Count - 1])
+				{
+					ending = true;
+					heart.end = true;
+					dialogue.gameObject.SetActive(true);
+					Talk();
+				}
+				else
+				{
+					counter--;
+				}
+			}
+			stress.text = "Stress: " + (100 - heart.Frequency);
 			compassion.text = "Compassion: " + (100 - heart.Frozen);
 			love.text = "Love: " + (heart.Love);
 		}
-        
+
 	}
 
 	/// <summary>
@@ -247,106 +268,114 @@ public class GameController : MonoBehaviour {
 	/// Types the given message on screen. 
 	/// </summary>
 	/// <returns>The text.</returns>
-    IEnumerator TypeText(string message)
-    {
-        if (!cracking)
-        {
-            typing = true;
-            string current = "";
-            dialogue.text = "";
-            char[] m = message.ToCharArray();
-            for (int i = 0; i < message.Length; i++)
-            {
-                if (skip)
-                {
-                    dialogue.text = message;
-                    skip = false;
-                    break;
-                }
-                current += m[i];
-                dialogue.text = current;
-                dialogue.text += "<color=#00000000>";
-                for (int j = i + 1; j < message.Length; j++)
-                {
-                    dialogue.text += m[j];
-                }
-                dialogue.text += "</color>";
-                yield return new WaitForSeconds(letterPause);
-            }
-            typing = false;
-        }
-    }
+	IEnumerator TypeText(string message)
+	{
+		if (!cracking)
+		{
+			typing = true;
+			string current = "";
+			dialogue.text = "";
+			char[] m = message.ToCharArray();
+			for (int i = 0; i < message.Length; i++)
+			{
+				if (skip)
+				{
+					dialogue.text = message;
+					skip = false;
+					break;
+				}
+				current += m[i];
+				dialogue.text = current;
+				dialogue.text += "<color=#00000000>";
+				for (int j = i + 1; j < message.Length; j++)
+				{
+					dialogue.text += m[j];
+				}
+				dialogue.text += "</color>";
+				yield return new WaitForSeconds(letterPause);
+			}
+			typing = false;
+		}
+	}
 
-    public void Talk()
-    {
-        if (typing)
-        {
-            skip = true;
-        }
-        else if ((location == 6 || location == 10 || location == 11 || location == 17) && heart.Love > 0 && !cracking)
-        {
-			if (!(location == 17) || heart.Love > 10) {
+	public void Talk()
+	{
+		if (typing)
+		{
+			skip = true;
+		}
+		else if ((location == 6 || location == 10 || location == 11 || location == 17) && heart.Love > 0 && !cracking)
+		{
+			if (!(location == 17) || heart.Love > 10)
+			{
 				cracking = true;
-				StartCoroutine (Crack ());
-			} else {
+				StartCoroutine(Crack());
+			}
+			else
+			{
 				heart.Love = 0;
 			}
-        }
+		}
 		else if (!cracking && location < dialogueTexts.Count)
-        {
-            StartCoroutine(TypeText(dialogueTexts[location]));
-			if (location == 0 && !space.enabled) {
+		{
+			StartCoroutine(TypeText(dialogueTexts[location]));
+			if (location == 0 && !space.enabled)
+			{
 				space.enabled = true;
-				StartCoroutine (PressSpace ());
-			} else if (location == 1 && space.enabled) {
+				StartCoroutine(PressSpace());
+			}
+			else if (location == 1 && space.enabled)
+			{
 				space.enabled = false;
 				pressSpace = false;
 				fadeIn = false;
 				fadeOut = false;
 			}
-            location++;
-        }
-        if (location == 18)
-        {
-            heart.Frequency = 80;
-        }
-    }
+			location++;
+		}
+		if (location == 18)
+		{
+			heart.Frequency = 80;
+		}
+	}
 
-    public IEnumerator Crack()
-    {
-        dialogue.text = "";
-        yield return new WaitForSeconds(0.5f);
-        heart.Love -= 25;
-        heart.Frequency -= 10;
-        if (heart.Frequency < 10)
-        {
-            heart.Frequency = 10;
-        }
-        heart.Crack();
-        if(heart.cracks == 4)
-        {
-            dialogue.gameObject.SetActive(false);
-        }
-        yield return new WaitForSeconds(1f);
-        cracking = false;
-        StartCoroutine(TypeText(dialogueTexts[location]));
-        location++;
-    }
+	public IEnumerator Crack()
+	{
+		dialogue.text = "";
+		yield return new WaitForSeconds(0.5f);
+		heart.Love -= 25;
+		heart.Frequency -= 10;
+		if (heart.Frequency < 10)
+		{
+			heart.Frequency = 10;
+		}
+		heart.Crack();
+		if (heart.cracks == 4)
+		{
+			dialogue.gameObject.SetActive(false);
+		}
+		yield return new WaitForSeconds(1f);
+		cracking = false;
+		StartCoroutine(TypeText(dialogueTexts[location]));
+		location++;
+	}
 
-    public IEnumerator End()
-    {
-        location++;
-        yield return new WaitForSeconds(6.0f);
-        SceneManager.LoadScene("Menu");
-    }
+	public IEnumerator End()
+	{
+		location++;
+		yield return new WaitForSeconds(6.0f);
+		SceneManager.LoadScene("Menu");
+	}
 
-	public IEnumerator Arrows() {
-		yield return new WaitForSeconds (0.5f);
+	public IEnumerator Arrows()
+	{
+		yield return new WaitForSeconds(0.5f);
 		fadeIn = true;
 	}
 
-	private IEnumerator PressSpace() {
-		yield return new WaitForSeconds (4.0f);
+	private IEnumerator PressSpace()
+	{
+		yield return new WaitForSeconds(4.0f);
 		pressSpace = true;
 		fadeIn = true;
 	}
